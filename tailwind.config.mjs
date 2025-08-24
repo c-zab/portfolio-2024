5187916e4e976a52c3bc7f2f4ad8e6ddf5bfdfa5
CarlosZ <zabaletacopa@gmail.com>
2025-08-24 02:21:01 -0400

    dark theme added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
